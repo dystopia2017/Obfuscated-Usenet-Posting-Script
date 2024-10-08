f2d047cb5faa1d9bd62100f5b36f0b141ba78318
dystopia2017 <30868809+dystopia2017@users.noreply.github.com>
2024-10-08 18:30:38 +0000

    Add files via upload

---
+++ _Toolset_/Apps/Usenet_Login.bat
@@ -0,0 +1,4 @@
+set serv="news.newsserver.com"                                                  
+set port="563"                                                                  
+set user="my_username"                                                          
+set pass="my_password" 
