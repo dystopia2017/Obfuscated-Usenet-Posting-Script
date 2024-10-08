33bb8c483fb130a76ce620bf3827e353d46d44d6
dystopia2017 <30868809+dystopia2017@users.noreply.github.com>
2024-10-08 18:33:25 +0000

    Delete _Toolset_/Apps directory

--- _Toolset_/Apps/Usenet_Login.bat
+++
@@ -1,4 +0,0 @@
-set serv="news.newsserver.com"                                                  
-set port="563"                                                                  
-set user="my_username"                                                          
-set pass="my_password" 
