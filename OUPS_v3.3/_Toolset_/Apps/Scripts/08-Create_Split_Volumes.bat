02904ae907c217a77ba1d932d42a47be4c71b773
dystopia2017 <30868809+dystopia2017@users.noreply.github.com>
2025-10-12 07:44:26 +0000

    Update 08-Create_Split_Volumes.bat
    Changed rar4 output to rar5 output.
    Rar4 is no longer compatible with new versions of WinRAR

--- OUPS_v3.3/_Toolset_/Apps/Scripts/08-Create_Split_Volumes.bat
+++ OUPS_v3.3/_Toolset_/Apps/Scripts/08-Create_Split_Volumes.bat
@@ -3,8 +3,9 @@ title %Name% ^| Packing into %vol% Volumes
 if not exist %wrar%   (goto Pack_With_7za)
 if /i "%urar%"=="Yes" (goto Pack_With_rar) else (goto Pack_With_7za)
 :Pack_With_rar
-%wrar% a -hp"%rpwd%" -m%rcom% -vn -v%vol% -ma4 "%Post%\%flnm%\%flnm%.rar" %inpt%
+%wrar% a -hp"%rpwd%" -m%rcom% -vn -v%vol% -m5 "%Post%\%flnm%\%flnm%.rar" %inpt%
 goto endpack
 :Pack_With_7za
 %Apps%\7za a -v%vol% -mhe=on -mx=%zcom% -p"%rpwd%" "%Post%\%flnm%\%flnm%.7z" %Post%\%flnm%\%inpt%
+
 :endpack
