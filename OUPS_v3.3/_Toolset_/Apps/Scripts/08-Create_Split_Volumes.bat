2523751d3ce6da60903295089f5b377c403bdc31
dystopia2017 <30868809+dystopia2017@users.noreply.github.com>
2025-10-18 07:29:38 +0000

    Update 08-Create_Split_Volumes.bat
    Adding a new check to see if a rar6.exe exists in your WinRAR install directory and using it if rar mode is enabled, Also tidies up the batch label names and flow.

--- OUPS_v3.3/_Toolset_/Apps/Scripts/08-Create_Split_Volumes.bat
+++ OUPS_v3.3/_Toolset_/Apps/Scripts/08-Create_Split_Volumes.bat
@@ -1,10 +1,22 @@
 @echo off&&title %Name% ^| Creating Split Volumes
 title %Name% ^| Packing into %vol% Volumes
+
+:Rar_Check
+if /i not "%urar%"=="Yes" (goto Pack_With_7za)
+if exist %orar% (goto PaPack_With_RAR_Four) else (goto Pack_With_RAR_Five)
+
+:Pack_With_RAR_Four
+if not exist %orar%   (goto Pack_With_7za)
+%orar% a -hp"%rpwd%" -m%rcom% -vn -v%vol% -ma4 "%Post%\%flnm%\%flnm%.rar" %inpt%
+goto endpack
+
+:Pack_With_RAR_Five
 if not exist %wrar%   (goto Pack_With_7za)
-if /i "%urar%"=="Yes" (goto Pack_With_rar) else (goto Pack_With_7za)
-:Pack_With_rar
 %wrar% a -hp"%rpwd%" -m%rcom% -vn -v%vol% -m5 "%Post%\%flnm%\%flnm%.rar" %inpt%
 goto endpack
+
 :Pack_With_7za
 %Apps%\7za a -v%vol% -mhe=on -mx=%zcom% -p"%rpwd%" "%Post%\%flnm%\%flnm%.7z" %Post%\%flnm%\%inpt%
+goto endpack
+
 :endpack
